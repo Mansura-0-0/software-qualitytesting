eb9207f78d6974a996858ca19288f0f7e9fc06a6
MansuraKhajra-STUDEN <MansuraKhajra-STUDEN@S-5M1H504-SL02>
2026-04-14 11:55:33 +0100

    Add project files.

---
+++ ParkingService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Project
+{
+    public class ParkingService
+    {
+        private readonly IDiscountService _discountService;
+
+        public ParkingService(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        public double CalculateFee(int hours, string vehicleType)
+        {
+            double fee;
+
+            if (vehicleType == "standard")
+                if ((hours >= 1) && (hours < 3))
+                    fee = hours * 4.0;
+                else if (hours >= 4)
+                    fee = hours * 3.0;
+                else
+                    fee = 0.0;
+            else if (vehicleType == "electric")
+                if ((hours >= 1) && (hours <= 5))
+                    fee = hours * 3.0;
+                else if (hours >= 6)
+                    fee = hours * 2.0;
+                else
+                    fee = 0.0;
+            else
+                fee = 0.0;
+
+            double discount = _discountService.GetDiscount();
+
+            if (hours >= 10)
+                fee = fee * discount;
+
+            return fee;
+        }
+    }
+
+    public interface IDiscountService
+    {
+        double GetDiscount();
+    }
+
+    public class DiscountService : IDiscountService
+    {
+        public double GetDiscount()
+        {
+            return 0.9;
+        }
+    }
+}
